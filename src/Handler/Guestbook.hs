76696b8ced7834706db862c21886793a3a366dba
cheater <195390+cheater@users.noreply.github.com>
2019-09-09 10:03:18 +0200

    Guestbook ajax and persistence initial version.

--- src/Handler/Guestbook.hs
+++ src/Handler/Guestbook.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Handler.Guestbook where
 
 import Import
+import Data.HashMap.Strict as H (lookup)
 
 postGuestbookR :: Handler Value
 postGuestbookR = do
     liftIO $ Prelude.putStrLn "starting postGuestbookR"
     -- requireJsonBody will parse the request body into the appropriate type, or return a 400 status code if the request JSON is invalid.
     -- (The ToJSON and FromJSON instances are derived in the config/models file).
-    guestbook <- (requireJsonBody :: Handler Guest)
+    guestbook <- (requireJsonBody :: Handler Value)
     liftIO $ Prelude.putStrLn ("parsed json body.")
-    -- liftIO $ Prelude.putStrLn ("json body: " Prelude.++ (show guestbook))
+    liftIO $ Prelude.putStrLn ("json body: " Prelude.++ (show guestbook))
 
     -- The YesodAuth instance in Foundation.hs defines the UserId to be the type used for authentication.
     maybeCurrentUserId <- maybeAuthId
@@ -18,7 +21,19 @@ postGuestbookR = do
     case maybeCurrentUserId of
         Nothing -> permissionDenied "Can't add guests if not logged in."
         Just currentUserId -> do
-            let guestbook' = guestbook { guestCreator = currentUserId }
+            case guestbook of
+                Object guestbookH -> do
+                    liftIO $ Prelude.putStrLn "guestbook':"
+                    liftIO $ Prelude.putStrLn $ (show guestbookH)
+                    let
+                        mName = H.lookup "name" guestbookH
+                        mEmail = H.lookup "email" guestbookH
+                    case (mName, mEmail) of
+                        (Just (String name), Just (String email)) -> do
+                            let guest = Guest { guestEmail = email, guestName = name, guestCreator = currentUserId }
+                            insertedGuest <- runDB $ insertEntity guest
+                            returnJson insertedGuest
 
-            insertedGuestbook <- runDB $ insertEntity guestbook'
-            returnJson insertedGuestbook
+                        _ -> invalidArgs ["Invalid JSON payload"]
+
+                _ -> invalidArgs ["Invalid JSON payload"]
